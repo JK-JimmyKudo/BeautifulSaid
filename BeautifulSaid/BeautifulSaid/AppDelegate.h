d7c638259737d3be905487d393f2590dbbcd8b18
pengwei <>
2018-08-31 14:45:41 +0800

    初始化项目

---
+++ BeautifulSaid/BeautifulSaid/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeautifulSaid
+//
+//  Created by power on 2018/8/31.
+//  Copyright © 2018年 BeautifulSaid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BeautifulSaid/BeautifulSaid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeautifulSaid
+//
+//  Created by power on 2018/8/31.
+//  Copyright © 2018年 BeautifulSaid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
